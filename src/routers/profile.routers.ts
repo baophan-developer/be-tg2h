e7ef6acc67330bc4caf866abc8a6e1d3f5658b8d
baophan-developer <baophan.developer@gmail.com>
2023-09-25 21:54:34 +0700

    fix: add router remove and edit address user

--- src/routers/profile.routers.ts
+++ src/routers/profile.routers.ts
@@ -2,7 +2,9 @@ import express, { Router } from "express";
 import { uploadMemories } from "../configs/upload.config";
 import {
     addAddressUser,
+    editAddressUser,
     getProfile,
+    removeAddressUser,
     updateAvatar,
     updateProfile,
 } from "../controllers/profile.controller";
@@ -12,6 +14,8 @@ const profileRouters: Router = express.Router();
 profileRouters.get("/", getProfile);
 profileRouters.put("/update-profile", updateProfile);
 profileRouters.put("/update-avatar", uploadMemories.single("avatar"), updateAvatar);
-profileRouters.put("/update-address", addAddressUser);
+profileRouters.post("/create-address", addAddressUser);
+profileRouters.put("/update-address", editAddressUser);
+profileRouters.delete("/delete-address", removeAddressUser);
 
 export default profileRouters;
