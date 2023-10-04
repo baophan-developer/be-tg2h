f7d91d2304669171b4711db148e2a40a12107c22
baophan-developer <baophan.developer@gmail.com>
2023-10-04 11:33:29 +0700

    populate product and remove line empty

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -7,42 +7,25 @@ import decodeToken from "../utils/decode-token";
 import { uploadMultipleHandler } from "../configs/upload.config";
 import { MSG_CREATE_PRODUCT_SUCCESS } from "../constants/messages";
 
-interface IFilterProduct {
-    owner: Schema.Types.ObjectId;
-    name: String;
-    price: {
-        /** lte: less than or equal */
-        lte: number;
-        /** gte: greater than or equal */
-        gte: number;
-    };
-    newness: number;
-    screen: {
-        sizeScreen: Schema.Types.ObjectId;
-        scanFrequencyScreen: Schema.Types.ObjectId;
-        resolutionScreen: Schema.Types.ObjectId;
-    };
-    ram: {
-        typeRam: Schema.Types.ObjectId;
-        capacityRam: Schema.Types.ObjectId;
-    };
-    rom: {
-        typeRom: Schema.Types.ObjectId;
-        capacityRom: Schema.Types.ObjectId;
-    };
-    gpu: Schema.Types.ObjectId;
-    cpu: Schema.Types.ObjectId;
-    os: Schema.Types.ObjectId;
-    brand: Schema.Types.ObjectId;
-    category: Schema.Types.ObjectId;
-    approve: boolean;
-    status: boolean;
-}
+interface IFilterProduct {}
 
 export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const {} = req.body as {};
-        const products = await ProductModel.find({});
+        const removeAttributePopulated = "-__v -createdAt -updatedAt";
+        const products = await ProductModel.find({})
+            .populate("sizeScreen", removeAttributePopulated)
+            .populate("scanFrequency", removeAttributePopulated)
+            .populate("resolutionScreen", removeAttributePopulated)
+            .populate("typeRam", removeAttributePopulated)
+            .populate("capacityRam", removeAttributePopulated)
+            .populate("typeRom", removeAttributePopulated)
+            .populate("capacityRom", removeAttributePopulated)
+            .populate("gpu", removeAttributePopulated)
+            .populate("cpu", removeAttributePopulated)
+            .populate("os", removeAttributePopulated)
+            .populate("category", removeAttributePopulated)
+            .populate("brand", removeAttributePopulated);
         return res.json({ list: products });
     } catch (error: any) {
         return next(new ResponseError(error.status, error.message));
@@ -53,13 +36,9 @@ export const createProduct = async (req: Request, res: Response, next: NextFunct
     try {
         const productInfo = req.body as IProduct;
         const { userId } = decodeToken(req);
-
         const product = await ProductModel.create({ ...productInfo, owner: userId });
-
         const images = await uploadMultipleHandler(req, res);
-
         await ProductModel.findByIdAndUpdate(product._id, { $set: { images: images } });
-
         return res.json({ message: MSG_CREATE_PRODUCT_SUCCESS });
     } catch (error: any) {
         const { status, message } = handleError(error);
