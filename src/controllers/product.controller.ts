32614daf74bd8b9d4ac57abdc7d22a3f6935d767
baophan-developer <baophan.developer@gmail.com>
2023-10-05 16:35:15 +0700

    add filter, pagination and sort

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,20 +1,71 @@
-import { Schema } from "mongoose";
 import { Request, Response, NextFunction } from "express";
 import ResponseError from "../utils/error-api";
-import ProductModel, { IProduct } from "../models/Product";
 import handleError from "../utils/handle-error";
 import decodeToken from "../utils/decode-token";
+import ProductModel, { IProduct } from "../models/Product";
 import { uploadMultipleHandler } from "../configs/upload.config";
 import { MSG_CREATE_PRODUCT_SUCCESS } from "../constants/messages";
 
-interface IFilterProduct {}
+interface IQueryProduct {
+    filters: any;
+    pagination: {
+        page: number;
+        limit: number;
+    };
+    /** Only sort on two attribute is approve and price */
+    sort: any;
+}
 
 const removeAttributePopulated = "-__v -createdAt -updatedAt";
 
 export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {} = req.body as {};
-        const products = await ProductModel.find({})
+        const { filters, pagination, sort } = req.body as IQueryProduct;
+
+        const defaultPage = 0;
+        const defaultLimit = 10;
+
+        const limit = pagination?.limit || defaultLimit;
+        const skip = pagination?.page * pagination?.limit || defaultPage;
+
+        const products = await ProductModel.find(filters, null, {
+            skip: skip,
+            limit: limit,
+            sort,
+        })
+            .populate("sizeScreen", removeAttributePopulated)
+            .populate("scanFrequency", removeAttributePopulated)
+            .populate("resolutionScreen", removeAttributePopulated)
+            .populate("typeRam", removeAttributePopulated)
+            .populate("capacityRam", removeAttributePopulated)
+            .populate("typeRom", removeAttributePopulated)
+            .populate("capacityRom", removeAttributePopulated)
+            .populate("gpu", removeAttributePopulated)
+            .populate("cpu", removeAttributePopulated)
+            .populate("os", removeAttributePopulated)
+            .populate("category", removeAttributePopulated)
+            .populate("brand", removeAttributePopulated)
+            .exec();
+
+        return res.json({
+            list: products,
+            total: products.length,
+            page: pagination.page || defaultPage,
+            limit: pagination.limit || defaultLimit,
+        });
+    } catch (error: any) {
+        return next(new ResponseError(error.status, error.message));
+    }
+};
+
+export const getDetailProduct = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { id } = req.params;
+        const product = await ProductModel.findById(id)
             .populate("sizeScreen", removeAttributePopulated)
             .populate("scanFrequency", removeAttributePopulated)
             .populate("resolutionScreen", removeAttributePopulated)
@@ -27,7 +78,7 @@ export const getProducts = async (req: Request, res: Response, next: NextFunctio
             .populate("os", removeAttributePopulated)
             .populate("category", removeAttributePopulated)
             .populate("brand", removeAttributePopulated);
-        return res.json({ list: products });
+        return res.json({ item: product });
     } catch (error: any) {
         return next(new ResponseError(error.status, error.message));
     }
@@ -54,28 +105,25 @@ export const createProduct = async (req: Request, res: Response, next: NextFunct
     }
 };
 
-export const getDetailProduct = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
+export const updateProduct = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { id } = req.params;
-        const product = await ProductModel.findById(id);
-        // .populate("sizeScreen", removeAttributePopulated)
-        // .populate("scanFrequency", removeAttributePopulated)
-        // .populate("resolutionScreen", removeAttributePopulated)
-        // .populate("typeRam", removeAttributePopulated)
-        // .populate("capacityRam", removeAttributePopulated)
-        // .populate("typeRom", removeAttributePopulated)
-        // .populate("capacityRom", removeAttributePopulated)
-        // .populate("gpu", removeAttributePopulated)
-        // .populate("cpu", removeAttributePopulated)
-        // .populate("os", removeAttributePopulated)
-        // .populate("category", removeAttributePopulated)
-        // .populate("brand", removeAttributePopulated);
-        return res.json({ item: product });
-    } catch (error: any) {
-        return next(new ResponseError(error.status, error.message));
-    }
+        const {} = req.body as IProduct;
+    } catch (error) {}
+};
+
+export const approveProduct = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.body;
+        await ProductModel.findByIdAndUpdate(
+            id,
+            { $set: { approve: true } },
+            { new: true }
+        );
+    } catch (error) {}
+};
+
+export const rejectProduct = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // delete product and reason reject
+    } catch (error) {}
 };
