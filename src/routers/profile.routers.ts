19ae97690606d0b0ff8327c10c667cf5254e74c4
baophan-developer <baophan.developer@gmail.com>
2023-10-24 21:21:50 +0700

    feature favorites product

--- src/routers/profile.routers.ts
+++ src/routers/profile.routers.ts
@@ -2,10 +2,12 @@ import express, { Router } from "express";
 import { uploadMemories } from "../configs/upload.config";
 import {
     addAddressUser,
+    addProductToFavorites,
     chooseAddressIsMain,
     editAddressUser,
     getProfile,
     removeAddressUser,
+    removeProductToFavorites,
     updateAvatar,
     updateProfile,
 } from "../controllers/profile.controller";
@@ -19,5 +21,7 @@ profileRouters.post("/create-address", addAddressUser);
 profileRouters.put("/update-address", editAddressUser);
 profileRouters.delete("/delete-address/:id", removeAddressUser);
 profileRouters.post("/choose-address-is-main", chooseAddressIsMain);
+profileRouters.put("/add-favorites", addProductToFavorites);
+profileRouters.put("/remove-favorites", removeProductToFavorites);
 
 export default profileRouters;
